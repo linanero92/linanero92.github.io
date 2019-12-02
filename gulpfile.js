588b10a260f97fba790e140782e04b9a34f8d123
Alina <linakuptsova92@gmail.com>
2019-12-02 22:59:26 +0300

    add gulp tasks minify-css & rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp  = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function (done) {
   console.log('Привет, мир!');
@@ -11,14 +12,17 @@ gulp.task('hello', function (done) {
 gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: "./src"
     }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 gulp.task('minify-css', function() {
-  return gulp.src('style/*.css')
+  return gulp.src("./src/css/*.css")
     .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('/out'));
+        .pipe(rename({
+          suffix: '.min'
+        }))
+    .pipe(gulp.dest("./src/css"));
 });
