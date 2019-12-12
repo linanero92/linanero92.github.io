8416cf96e689757a8c05e65b953449707e5a88e4
Alina <linakuptsova92@gmail.com>
2019-12-13 02:31:02 +0300

    feat: add close function on press escape and on the window

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
+
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
 
   closeBtn.addEventListener('click', switchModal);
 
-});
+  document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 27) {switchModal() }
+  });
+
+  document.addEventListener('click', function(e) {
+    e.target.classList.toggle('modal--visible')
+    });
+  
+  });
