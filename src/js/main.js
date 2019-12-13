fd8d5f3320b70e0c91d187ceb149a6bbf962ae3a
Alina <linakuptsova92@gmail.com>
2019-12-13 18:08:29 +0300

    feat: add jquery functions to modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -17,7 +18,34 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
 
   document.addEventListener('click', function(e) {
-    e.target.classList.toggle('modal--visible')
+    e.target.classList.toggle('.modal--visible')
     });
   
   });
+*/
+
+$(document).ready(function () {
+ 
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
+  
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  $(document).keydown(function () {
+    if (event.keyCode == 27) {
+      modal.removeClass('modal--visible');
+    }
+  }); 
+
+  $(document).click(function (event) {
+    $(event.target).toggleClass('modal--visible');
+  });
+   
+});
