ead3090398ed4be50b8b12e96056cb2c03a09c45
Alina <linakuptsova92@gmail.com>
2019-12-19 23:07:07 +0300

    add map and direction blocks

--- src/js/main.js
+++ src/js/main.js
@@ -158,6 +158,40 @@ $(document).ready(function () {
   
   //маска для номера телефона
 
-  $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7(___) ___-__-__"});
+  $('[type=tel]').mask('+7(000) 000-00-00', { placeholder: "+7(___) ___-__-__" });
   
+  // создание yandex карты
+ ymaps.ready(function () {
+   var myMap = new ymaps.Map('map', {
+     center: [47.244734, 39.723227],
+     zoom: 9
+   }, {
+     searchControlProvider: 'yandex#search'
+   }),
+
+     // Создаём макет содержимого.
+     MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+       '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+     ),
+
+     myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+       hintContent: 'Наш офис',
+       balloonContent: 'Вход со двора'
+     }, {
+       // Опции.
+       // Необходимо указать данный тип макета.
+       iconLayout: 'default#image',
+       // Своё изображение иконки метки.
+       iconImageHref: 'img/location-pin.png',
+       // Размеры метки.
+       iconImageSize: [32, 32],
+       // Смещение левого верхнего угла иконки относительно
+       // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+     });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
+
 });
